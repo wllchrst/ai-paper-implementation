c64619ea40cedcd1b08fa342bdf47dc0e7f29272
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-02-23 01:55:59 +0700

    feat: add notes + data loader

--- code/data_loader.py
+++ code/data_loader.py
@@ -1,13 +1,29 @@
 import torchvision
-
-from torchvision.transforms import transforms
 from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+from typing import Tuple
 
-def load_MNIST_dataset() -> dict[str, DataLoader]:
+def load_MNIST_dataset(batch_size=8) -> Tuple[DataLoader, DataLoader]:
+    """Load MNIST Dataset from torchvision library
+
+    Args:
+    - batch_size: int Batch size for the loader
+
+    Returns:
+        Tuple[DataLoader, DataLoader]: Train Loader and Test Loader
+    """
     transform = transforms.Compose\
         (transforms=[transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
     training_dataset = torchvision.datasets.\
         MNIST(root='../data', train=True, download=True, transform=transform)
 
-    return {}
+    test_dataset = torchvision.datasets.\
+        MNIST(root='../data', train=False, download=True, transform=transform)
+
+    train_loader = DataLoader\
+        (training_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader\
+        (test_dataset, batch_size=batch_size, shuffle=True)
+        
+    return train_loader, test_loader
