c298738ca97b9ed80069c6b1bd5afef85254d0b4
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-02-23 19:01:49 +0700

    feat: add notes and more layer

--- code/model.py
+++ code/model.py
@@ -1,20 +1,64 @@
 import torch.nn as nn
 
 class AlexNet(nn.Module):
-    """Class for VVGG 16 Architecture
+    """
+    Class for Alex Net Architecture
 
-    Args:
-        nn (_type_): _description_
+    Notes for the alex net architecture.
+    - ReLU Layer is applied to every Convolutional and Full Connected Layer
+    - Local Response Normalization is used on the first and second Convolutional Layer.
+    - Max Pooling Layer always follow Local Response Normalization layer and on the fifth convolutional layer.
+    - Paper doesn't specify the stride for every conv layer, only explain the first conv layer stride.
     """
     def __init__(self, input_channel):
-        super(AlexNet, self).__init__() 
-        self.first_conv_layer = nn.Conv2d\
-            (in_channels=input_channel, out_channels=96, kernel_size=11, stride=4)
+        super(AlexNet, self).__init__()
+        self.local_response_norm_size = 2
+
+        self.first_layer = nn.Sequential(
+            nn.Conv2d(in_channels=input_channel, out_channels=96, kernel_size=11, stride=4),
+            nn.LocalResponseNorm(self.local_response_norm_size, k=2, alpha=10e4, beta=0.75),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+        self.second_layer = nn.Sequential(
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1),
+            nn.LocalResponseNorm(self.local_response_norm_size, k=2, alpha=10e4, beta=0.75),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+        self.third_layer = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1),
+            nn.ReLU(),
+        )
+
+        self.fourth_layer = nn.Sequential(
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1),
+            nn.ReLU(),
+        )
+
+        self.fifth_layer = nn.Sequential(
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
 
     def forward(self, inputs):
-        print(inputs.shape)
-        inputs = self.first_conv_layer(inputs)
+        print(f'Initial shape {inputs.shape}')
 
-        print(f"First Layers: {inputs.shape}")
+        inputs = self.first_layer(inputs)
+        print(f'Shape after First Layer: {inputs.shape}')
 
+        inputs = self.second_layer(inputs)
+        print(f'Shape after First Layer: {inputs.shape}')
+
+        inputs = self.third_layer(inputs)
+        print(f'Shape after First Layer: {inputs.shape}')
+
+        inputs = self.fourth_layer(inputs)
+        print(f'Shape after First Layer: {inputs.shape}')
+
+        inputs = self.fifth_layer(inputs)
+        print(f'Shape after First Layer: {inputs.shape}')
         return inputs
