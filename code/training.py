6e1d458a399dd7c91bcabee73302ed0f839a83a6
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-02-23 19:02:32 +0700

    feat: training with test

--- code/training.py
+++ code/training.py
@@ -1,12 +1,30 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
-def train(
+def test_model(
+    model: nn.Module,
+    test_loader: DataLoader
+    ) -> None:
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch in test_loader:
+            images, labels = batch
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy: {correct / total}')
+
+def train_model(
     model: nn.Module,
     train_loader: DataLoader,
     test_loader: DataLoader,
     num_epochs=3,
+    with_test=True
     ) -> None:
 
     optimizer = optim.Adam(model.parameters(), lr=0.001)
@@ -22,6 +40,5 @@ def train(
             loss.backward()
             optimizer.step()
 
-
-    for batch in test_loader:
-        
+    if with_test:
+        test_model(model, test_loader)
