afd77adff74e71d5969646e3d6c9d3600486723c
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-02-24 10:05:51 +0700

    feat: add dropout and fully connected layer

--- code/model.py
+++ code/model.py
@@ -16,6 +16,7 @@ class AlexNet(nn.Module):
 
         self.first_layer = nn.Sequential(
             nn.Conv2d(in_channels=input_channel, out_channels=96, kernel_size=11, stride=4),
+            nn.Dropout2d(),
             nn.LocalResponseNorm(self.local_response_norm_size, k=2, alpha=10e4, beta=0.75),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
@@ -23,6 +24,7 @@ class AlexNet(nn.Module):
 
         self.second_layer = nn.Sequential(
             nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1),
+            nn.Dropout2d(),
             nn.LocalResponseNorm(self.local_response_norm_size, k=2, alpha=10e4, beta=0.75),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
@@ -44,6 +46,11 @@ class AlexNet(nn.Module):
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
 
+        self.fully_connected = nn.Sequential(
+            nn.Linear(in_features=256, out_features=4096),
+            nn.Linear(in_features=4096, out_features=10)
+        )
+
     def forward(self, inputs):
         print(f'Initial shape {inputs.shape}')
 
@@ -51,14 +58,19 @@ class AlexNet(nn.Module):
         print(f'Shape after First Layer: {inputs.shape}')
 
         inputs = self.second_layer(inputs)
-        print(f'Shape after First Layer: {inputs.shape}')
+        print(f'Shape after Second Layer: {inputs.shape}')
 
         inputs = self.third_layer(inputs)
-        print(f'Shape after First Layer: {inputs.shape}')
+        print(f'Shape after Third Layer: {inputs.shape}')
 
         inputs = self.fourth_layer(inputs)
-        print(f'Shape after First Layer: {inputs.shape}')
+        print(f'Shape after Fourth Layer: {inputs.shape}')
 
         inputs = self.fifth_layer(inputs)
-        print(f'Shape after First Layer: {inputs.shape}')
-        return inputs
+        print(f'Shape after Fifth Layer: {inputs.shape}')
+
+        out = inputs.view(inputs.size(0), -1)
+
+        out = self.fully_connected(out)
+        print(f'Shape after Last Layer: {out.shape}')
+        return out
