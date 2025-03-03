d242077f286c24e4a0e2ef18b2e97fa9e3b9a9f0
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-03-03 21:21:04 +0700

    feat: organize files

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import torch
 import warnings
-from training import train_model
-from data_loader import load_cifar10_dataset
-from model import AlexNet
+from training_script import data_loader as dl 
+from training_script import training as t
+from alex_net import model as alex_net
 
 warnings.filterwarnings("ignore")
 
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Training will use device: {device}")
 
-    train_loader, test_loader = load_cifar10_dataset(show_image=False)
-    model = AlexNet(3).to(device)
+    train_loader, test_loader = dl.load_cifar10_dataset(show_image=False)
+    model = alex_net.AlexNet(3).to(device)
 
-    train_model(
+    t.train_model(
         model=model,
         train_loader=train_loader,
         test_loader=test_loader,
---
+++ training_script/resnet.py
@@ -0,0 +1,6 @@
+import torch.nn as nn
+
+class Resnet(nn.Module):
+    def __init__(self):
+        super(Resnet, self).__init__()
+        
